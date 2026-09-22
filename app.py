c588f6e49910ec58e82a41533b48270560d08b1f
rasin25725 <148531932+rasin25725@users.noreply.github.com>
2026-09-22 12:50:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import tensorflow as tf
 import joblib
 
 model = tf.keras.models.load_model("machine_temperature_rnn.keras")
-scaler = joblib.load("temperature_scaler.pkl")
+
+X_scaler = joblib.load("X_scaler.pkl")
+y_scaler = joblib.load("y_scaler.pkl")
 
 st.title("Machine Temperature Prediction")
 
@@ -15,14 +17,19 @@ temp2 = st.number_input("T22 Temperature")
 vib2 = st.number_input("T22 Vibration")
 
 if st.button("Predict"):
+
     new_data = np.array([
         [temp1, vib1],
         [temp2, vib2]
     ])
 
-    new_data_scaled = scaler.transform(new_data)
+    new_data_scaled = X_scaler.transform(new_data)
     new_data_scaled = new_data_scaled.reshape(1, 2, 2)
 
-    prediction = model.predict(new_data_scaled)
+    prediction_scaled = model.predict(new_data_scaled)
 
-    st.success(f"Predicted T23 Temperature: {prediction[0][0]:.2f} °C")
+    prediction = y_scaler.inverse_transform(prediction_scaled)
+
+    st.success(
+        f"Predicted T23 Temperature: {prediction[0][0]:.2f} °C"
+    )
