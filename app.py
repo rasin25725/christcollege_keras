02a8b5e50f01d5da93ca9ebe28765cc8e3c25d67
rasin25725 <148531932+rasin25725@users.noreply.github.com>
2026-09-22 12:39:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
+import joblib
 
 model = tf.keras.models.load_model("machine_temperature_rnn.keras")
+scaler = joblib.load("temperature_scaler.pkl")
 
-st.title("Machine Temperature Predictor")
+st.title("Machine Temperature Prediction")
 
-st.write("Enter the previous two machine readings:")
+temp1 = st.number_input("T21 Temperature")
+vib1 = st.number_input("T21 Vibration")
 
-temp1 = st.number_input("Previous Timestamp 1 - Temperature")
-vibration1 = st.number_input("Previous Timestamp 1 - Vibration")
+temp2 = st.number_input("T22 Temperature")
+vib2 = st.number_input("T22 Vibration")
 
-temp2 = st.number_input("Previous Timestamp 2 - Temperature")
-vibration2 = st.number_input("Previous Timestamp 2 - Vibration")
-
-if st.button("Predict Next Temperature"):
-
-    input_data = np.array([
-        [temp1, vibration1],
-        [temp2, vibration2]
+if st.button("Predict"):
+    new_data = np.array([
+        [temp1, vib1],
+        [temp2, vib2]
     ])
 
-    input_data = input_data.reshape(1, 2, 2)
+    new_data_scaled = scaler.transform(new_data)
+    new_data_scaled = new_data_scaled.reshape(1, 2, 2)
 
-    prediction = model.predict(input_data, verbose=0)
+    prediction = model.predict(new_data_scaled)
 
-    st.subheader("Predicted Next Machine Temperature:")
-
-    st.success(f"{prediction[0][0]:.2f} °C")
+    st.success(f"Predicted T23 Temperature: {prediction[0][0]:.2f} °C")
